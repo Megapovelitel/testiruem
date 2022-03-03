c138edb7726580b1d44528e3c6bb6f9876f05aa7
Megapovelitel <megapovelitel@yandex.com>
2022-03-03 19:21:15 +0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const requestListener = (req,res) => {
+  const {method, url, headers} = req;
+  const {pathname, searchParams} = new URL(url, `http://${headers.host}`)
+  console.log(url)
+  console.log(pathname)
+  res.writeHead(200)
+  res.end('<h1>hello world</h1>')
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
